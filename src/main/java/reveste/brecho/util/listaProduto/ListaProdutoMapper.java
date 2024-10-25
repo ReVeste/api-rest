ee371379f7f07fca35fc0e97ad588debcad8d1bd
Pedro Sarabando <143150417+Psarabando@users.noreply.github.com>
2024-10-25 00:29:40 -0300

    Desenvolvendo método do valor total do pedido
    Refatorando parte do endpoints de Pedido, adicionando o método para
    calcular o valor total do mesmo. Utilizando um vetor customizado para
    lidar com este método recursivo.

---
+++ src/main/java/reveste/brecho/util/listaProduto/ListaProdutoMapper.java
@@ -0,0 +1,24 @@
+package reveste.brecho.util.listaProduto;
+
+import reveste.brecho.dto.produto.ProdutoDTO;
+import reveste.brecho.dto.produto.ProdutoDetalheRespostaDto;
+
+import java.util.List;
+
+public class ListaProdutoMapper {
+
+    public static ListaProduto toListaProduto(List<ProdutoDTO> produtos){
+
+        if (produtos == null) return null;
+
+        ListaProduto produtosListados = new ListaProduto(produtos.size());
+
+        for (int i = 0; i < produtosListados.size(); i++) {
+            produtosListados.adiciona(produtos.get(i));
+        }
+
+        return produtosListados;
+
+    }
+
+}
