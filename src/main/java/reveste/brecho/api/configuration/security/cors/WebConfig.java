810d2598a84fc942b6b98ce37cdfe1dec54f29b6
ketelynMedina <ketelyn.martins@sptech.school>
2025-05-31 23:01:14 -0300

    Adicionando ips para acesso

--- src/main/java/reveste/brecho/api/configuration/security/cors/WebConfig.java
+++ src/main/java/reveste/brecho/api/configuration/security/cors/WebConfig.java
@@ -14,9 +14,12 @@ public class WebConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:3000"); // Especifique o frontend
-        config.addAllowedOrigin("http://107.23.40.115/"); // Especifique o frontend
-        config.addAllowedOrigin("http://10.0.2.2:8080"); // Mobile
+//        config.addAllowedOrigin("http://localhost:3000"); // Especifique o frontend
+//        config.addAllowedOrigin("http://107.23.40.115/"); // Especifique o frontend
+//        config.addAllowedOrigin("http://10.0.2.2:8080"); // Mobile
+        config.addAllowedOriginPattern("http://localhost:*");
+        config.addAllowedOriginPattern("http://10.0.1.*");
+        config.addAllowedOriginPattern("http://10.0.0.*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
 
