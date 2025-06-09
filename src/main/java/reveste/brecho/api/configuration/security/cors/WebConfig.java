e82a1e62042a1c6edcba11be23bf2447a0473aef
RyanMirandaBarbosa <ryan.barbosa@sptech.scholl>
2025-06-09 02:39:29 -0300

    alteração sprint 3

--- src/main/java/reveste/brecho/api/configuration/security/cors/WebConfig.java
+++ src/main/java/reveste/brecho/api/configuration/security/cors/WebConfig.java
@@ -14,20 +14,12 @@ public class WebConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(false);
-        config.addAllowedOrigin("http://localhost:3000"); // Especifique o frontend
-        config.addAllowedOrigin("http://localhost:8080"); // Especifique o frontend
-        config.addAllowedOrigin("https://black-meadow-0cd280a0f.4.azurestaticapps.net/"); // Especifique o frontend
+//        config.addAllowedOrigin("http://localhost:3000"); // Especifique o frontend
+        config.addAllowedOrigin("http://44.195.135.8"); // Especifique o frontend
         config.addAllowedOrigin("http://10.0.2.2:8080"); // Mobile
-        config.addAllowedOrigin("http://107.22.88.213"); // IP específico permitido
-        config.addAllowedOrigin("http://107.22.88.213:3000"); // IP específico permitido
-        config.addAllowedOrigin("http://107.22.88.213:8080"); // IP específico permitido
-        config.addAllowedOrigin("http://3.209.222.70"); // IP específico permitidocd
-
         config.addAllowedOriginPattern("http://localhost:*");
-        config.addAllowedOriginPattern("http://107.22.88.213:*"); // IP específico permitido
-        config.addAllowedOriginPattern("http://10.0.0.*"); // IP específico permitido
-        config.addAllowedOriginPattern("http://10.0.1.*"); // IP específico permitido
-
+        config.addAllowedOriginPattern("http://10.0.1.*");
+        config.addAllowedOriginPattern("http://10.0.0.*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
 
