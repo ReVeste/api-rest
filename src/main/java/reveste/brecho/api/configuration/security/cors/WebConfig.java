3d3e44e671c26256a4ac746d32e79a1ec0b54a84
RyanMirandaBarbosa <ryan.barbosa@sptech.scholl>
2025-06-09 03:01:34 -0300

    docker

--- src/main/java/reveste/brecho/api/configuration/security/cors/WebConfig.java
+++ src/main/java/reveste/brecho/api/configuration/security/cors/WebConfig.java
@@ -13,10 +13,13 @@ public class WebConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:3000"); // Especifique o frontend
-        config.addAllowedOrigin("https://black-meadow-0cd280a0f.4.azurestaticapps.net/"); // Especifique o frontend
+        config.setAllowCredentials(false);
+//        config.addAllowedOrigin("http://localhost:3000"); // Especifique o frontend
+        config.addAllowedOrigin("http://44.195.135.8"); // Especifique o frontend
         config.addAllowedOrigin("http://10.0.2.2:8080"); // Mobile
+        config.addAllowedOriginPattern("http://localhost:*");
+        config.addAllowedOriginPattern("http://10.0.1.*");
+        config.addAllowedOriginPattern("http://10.0.0.*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
 
