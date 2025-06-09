093e1cfca4832ed82aa6ee8e40c6ccece801d2c6
RyanMirandaBarbosa <ryan.barbosa@sptech.scholl>
2025-06-09 18:57:38 -0300

    alteração back end

--- src/main/java/reveste/brecho/controller/ProdutoController.java
+++ src/main/java/reveste/brecho/controller/ProdutoController.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/produtos")
+@RequestMapping("/api/cd")
 public class ProdutoController implements ProdutoSwagger {
 
     private final ProdutoService produtoService;
