85592a634032d9c97a968f81b42d6963e7f53eba
RyanMirandaBarbosa <ryan.barbosa@sptech.scholl>
2025-06-11 17:29:53 -0300

    commit

--- src/main/java/reveste/brecho/api/configuration/security/cors/WebConfig.java
+++ src/main/java/reveste/brecho/api/configuration/security/cors/WebConfig.java
@@ -15,8 +15,11 @@ public class WebConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(false);
 //        config.addAllowedOrigin("http://localhost:3000"); // Especifique o frontend
+
         config.addAllowedOrigin("http://44.195.135.8"); // Especifique o frontend
+
         config.addAllowedOrigin("http://10.0.2.2:8080"); // Mobile
+
         config.addAllowedOriginPattern("http://localhost:*");
         config.addAllowedOriginPattern("http://10.0.1.*");
         config.addAllowedOriginPattern("http://10.0.0.*");
